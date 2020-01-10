0499a196983106ee2fdb89e34a05ee2d7653c953
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-01-10 13:09:57 -0600

    Actualización Monumento.js

--- AppTlaco/screens/Monumento.js
+++ AppTlaco/screens/Monumento.js
@@ -33,13 +33,14 @@ const Monumento = props => {
 
     const cancelScan = () => {setModalState(false);}
 
+
     return (
         <ScrollView showsVerticalScrollIndicator = {false} style = {styles.contenedorComponentes}>
-
+    
             <MyQRScanner visible = {modalState} onCancel = {cancelScan}/>
-            
+                
             <ScrollView horizontal = {true} showsHorizontalScrollIndicator = {false} style = {{alignContent : 'center'}}>
-
+    
                 <View style = {{paddingEnd : 10}}>
                     <Image
                         style={styles.estiloImagen}
@@ -49,7 +50,7 @@ const Monumento = props => {
                         <Text>Por: Shivo dd</Text>
                     </View>
                 </View>
-
+    
                 <View style = {{paddingEnd : 10}}>
                     <Image
                         style={styles.estiloImagen}
@@ -59,8 +60,8 @@ const Monumento = props => {
                         <Text>Por: Shivo</Text>
                     </View>
                 </View>
-                
-
+                    
+    
                 <View style = {{paddingEnd : 10}}>
                     <Image
                         style={styles.estiloImagen}
@@ -70,67 +71,67 @@ const Monumento = props => {
                         <Text>Por: Shivo ll</Text>
                     </View>
                 </View>
-                
+                    
             </ScrollView>
-
+    
             <Titulo titulo = "Iglesia de San Miguel Arcángel"/>
-
+    
             <View style = {styles.contenedorTexto}>
                 <Text style = {styles.estiloSeparador}>HISTORIA</Text>
             </View>
-
+    
             <View >
                 <Text style = {styles.estiloTexto}>
-                Tlacotalpan perteneció al territorio Totonaca en el siglo XII, por lo que su fundación se remonta a esa época. 
-                Era cabecera de Atlizintla (hoy Alvarado), Xiuhbiapan, Ahuatcopan, Pozutlan y Tlazintlata, en el siglo XV, 
-                luego de tomar Cempoala y Cotaxtla, Axayácatl sometió al antiguo asentamiento indígena al Imperio azteca en 1475, 
-                en el contexto de la Conquista de Tochpan (Tuxpan) de 1480, y bautizó al asentamiento con el nombre de Tlācotālpan, 
-                que significa: entre aguas.
+                    Tlacotalpan perteneció al territorio Totonaca en el siglo XII, por lo que su fundación se remonta a esa época. 
+                    Era cabecera de Atlizintla (hoy Alvarado), Xiuhbiapan, Ahuatcopan, Pozutlan y Tlazintlata, en el siglo XV, 
+                    luego de tomar Cempoala y Cotaxtla, Axayácatl sometió al antiguo asentamiento indígena al Imperio azteca en 1475, 
+                    en el contexto de la Conquista de Tochpan (Tuxpan) de 1480, y bautizó al asentamiento con el nombre de Tlācotālpan, 
+                    que significa: entre aguas.
                 </Text>
             </View>
-
+    
             <View style = {styles.contenedorTexto}>
                 <Text style = {styles.estiloSeparador}>¿SABÍAS QUÉ?</Text>
             </View>
-
+    
             <View >
                 <Text style = {styles.estiloTexto}>
-                Tlacotalpan perteneció al territorio Totonaca en el siglo XII, por lo que su fundación se remonta a esa época. 
-                Era cabecera de Atlizintla (hoy Alvarado), Xiuhbiapan, Ahuatcopan, Pozutlan y Tlazintlata, en el siglo XV, 
-                luego de tomar Cempoala y Cotaxtla, Axayácatl sometió al antiguo asentamiento indígena al Imperio azteca en 1475, 
-                en el contexto de la Conquista de Tochpan (Tuxpan) de 1480, y bautizó al asentamiento con el nombre de Tlācotālpan, 
-                que significa: entre aguas.
+                    Tlacotalpan perteneció al territorio Totonaca en el siglo XII, por lo que su fundación se remonta a esa época. 
+                    Era cabecera de Atlizintla (hoy Alvarado), Xiuhbiapan, Ahuatcopan, Pozutlan y Tlazintlata, en el siglo XV, 
+                    luego de tomar Cempoala y Cotaxtla, Axayácatl sometió al antiguo asentamiento indígena al Imperio azteca en 1475, 
+                    en el contexto de la Conquista de Tochpan (Tuxpan) de 1480, y bautizó al asentamiento con el nombre de Tlācotālpan, 
+                    que significa: entre aguas.
                 </Text>
             </View>
-
+    
             <View style = {styles.contenedorTexto}>
                 <Text style = {styles.estiloSeparador}>¿SABÍAS QUÉ?</Text>
             </View>
-
+    
             <View >
                 <Text style = {styles.estiloTexto}>
-                Tlacotalpan perteneció al territorio Totonaca en el siglo XII, por lo que su fundación se remonta a esa época. 
-                Era cabecera de Atlizintla (hoy Alvarado), Xiuhbiapan, Ahuatcopan, Pozutlan y Tlazintlata, en el siglo XV, 
-                luego de tomar Cempoala y Cotaxtla, Axayácatl sometió al antiguo asentamiento indígena al Imperio azteca en 1475, 
-                en el contexto de la Conquista de Tochpan (Tuxpan) de 1480, y bautizó al asentamiento con el nombre de Tlācotālpan, 
-                que significa: entre aguas.
+                    Tlacotalpan perteneció al territorio Totonaca en el siglo XII, por lo que su fundación se remonta a esa época. 
+                    Era cabecera de Atlizintla (hoy Alvarado), Xiuhbiapan, Ahuatcopan, Pozutlan y Tlazintlata, en el siglo XV, 
+                    luego de tomar Cempoala y Cotaxtla, Axayácatl sometió al antiguo asentamiento indígena al Imperio azteca en 1475, 
+                    en el contexto de la Conquista de Tochpan (Tuxpan) de 1480, y bautizó al asentamiento con el nombre de Tlācotālpan, 
+                    que significa: entre aguas.
                 </Text>
             </View>
-
+    
             <View style = {styles.contenedorTexto}>
                 <Text style = {styles.estiloSeparador}>EVENTOS QUE SE CELEBRAN</Text>
             </View>
-
+    
             <View >
                 <Text style = {styles.estiloTexto}>
                 29 de septiembre se celebra
                 </Text>
             </View>
-
+    
             <View style = {styles.contenedorTexto}>
                 <Text style = {styles.estiloSeparador}>MULTIMEDIA</Text>
             </View>
-
+    
             <View style = {styles.contenedorVideo}>
                 <Video
                     source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
@@ -142,15 +143,16 @@ const Monumento = props => {
                     style={{ width: 300, height: 300, borderRadius: 8, borderWidth: 0.5 }}
                 />
             </View>
-
+    
             <View style = {styles.contenedorBotones}>
                 <Botones doThis = {callQRScanner} title = "ESCANEAR DE NUEVO"/>
                 <Botones title = "INICIO" />
             </View>
-
+    
             {comercial}
         </ScrollView>
     );
+
 };
 
 const styles = StyleSheet.create({
