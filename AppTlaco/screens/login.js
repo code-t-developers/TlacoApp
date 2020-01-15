0a05294ce4a6c05cd82f4eee6d97ab0fae2a1ff8
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-01-15 13:47:58 -0600

    Inicio de sesión con Facebook
    15/01/2020

--- AppTlaco/screens/login.js
+++ AppTlaco/screens/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import * as Google from 'expo-google-app-auth'
+import * as Google from 'expo-google-app-auth';
+import * as Facebook from 'expo-facebook';
 import {
   AppRegistry,
   StyleSheet,
@@ -7,7 +8,8 @@ import {
   View,
   ImageBackground,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert
 } from 'react-native';
 
 const background = require("../images/tlaco.jpg");
@@ -15,33 +17,68 @@ const lockIcon = require("../images/lock.png");
 const personIcon = require("../images/person.png");
 
 const Login = props =>{
-  state={signedIn: false,
+  state={
+    signedIn: false,
     name: "",
-    photoUrl: "",
+    photoUrl: "https://bootdey.com/img/Content/avatar/avatar6.png",
   }
-    const signIn = async () => {
-      try {
-        const result = await Google.logInAsync({
-          androidClientId:
-            "551422502591-oilem9p1igp26og11l61en8129f0l1o4.apps.googleusercontent.com",
+
+  const signIn = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: "551422502591-oilem9p1igp26og11l61en8129f0l1o4.apps.googleusercontent.com",
           //iosClientId: YOUR_CLIENT_ID_HERE,  <-- if you use iOS
-          scopes: ["profile", "email"]
-        })
+        scopes: ["profile", "email"]
+      })
   
-        if (result.type === "success") {
-            state.signedIn= true;
-            state.name=result.user.name;
-            state.photoUrl= result.user.photoUrl;
+      if (result.type === "success") {
+          state.signedIn= true;
+          state.name=result.user.name;
+          state.photoUrl= result.user.photoUrl;
           
-          console.log("Sesión iniciada", state.name)
           props.navigation.navigate({routeName: 'Perfil', otherParam: state.name, otherParam: state.photoUrl});
-        } else {
+      } else {
           console.log("cancelled")
-        }
-      } catch (e) {
-        console.log("error", e)
       }
+    } catch (e) {
+        console.log("error", e)
     }
+  }
+
+  
+  const LoginFB = async () => {
+    state = {
+      name : "",
+      photoUrl: ""
+    }
+    try {
+      await Facebook.initializeAsync('2553532308304922');
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=name,picture.type(large)`);
+        
+        const info = await response.json();
+        state.name = info.name;
+        state.photoUrl = info.picture.data.url;
+
+        props.navigation.navigate({routeName: 'Perfil', otherParam: state.name, otherParam: state.photoUrl});
+      } else {
+        // type === 'cancel'
+        Alert.alert("Ups!", "");
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
   return (
     <ImageBackground
     style={[styles.background, styles.container]} 
@@ -112,7 +149,7 @@ const Login = props =>{
     </View>
     </TouchableOpacity>
 
-    <TouchableOpacity activeOpacity={.5}>
+    <TouchableOpacity activeOpacity={.5} onPress = {LoginFB} >
     <View style={styles.button}>
     <Text style={styles.buttonText}>Ingresar Con Facebook</Text>
     </View>
