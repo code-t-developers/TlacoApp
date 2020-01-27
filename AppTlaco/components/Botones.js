da469ac037c6f9d3c2ffdd777e2053f7a199cc3c
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-01-27 11:41:24 -0600

    Actualización de los Botones.
    Se agregó la propiedad de que ahora los botones reciban una ruta de una imagen para ponerla como ícono.
    
    27/01/2020

--- AppTlaco/components/Botones.js
+++ AppTlaco/components/Botones.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Ionicons } from '@expo/vector-icons';
 
 
 const Botones = props => {
-    return (
-        <TouchableOpacity activeOpacity={.5} style = {{...styles.botones, ...props.style}} onPress = {props.doThis}>
-            <Ionicons name = {props.myIcon} size = {27} color = "white" style = {{margin: 5}}/>
-            <Text style = {{...styles.textoBotones, ...{fontSize : props.customFontSize}}}>{props.title}</Text>
-        </TouchableOpacity>
-    );
+    if(props.myIcon){
+        return (
+            <TouchableOpacity activeOpacity={.5} style = {{...styles.botones, ...props.style}} onPress = {props.doThis}>
+                <Ionicons name = {props.myIcon} size = {27} color = "white" style = {{margin: 5}}/>
+                <Text style = {{...styles.textoBotones, ...{fontSize : props.customFontSize}}}>{props.title}</Text>
+            </TouchableOpacity>
+        );
+    }else if(props.myImageIcon){
+        return (
+            <TouchableOpacity activeOpacity={.5} style = {{...styles.botones, ...props.style}} onPress = {props.doThis}>
+                <Image
+                        style = {{width : 30, height: 30}}
+                        source={props.myImageIcon}
+                />
+                <Text style = {{...styles.textoBotones, ...{fontSize : props.customFontSize}}}>{props.title}</Text>
+            </TouchableOpacity>
+        );    
+    }
+
 };
 
 const styles = StyleSheet.create({
